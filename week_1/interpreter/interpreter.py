16325b4f5b6e27f70bd2795ff01c5a12464e45c6
Khalid Azaanoun <123247514+yamilobt8@users.noreply.github.com>
2024-10-19 22:47:40 +0100

    CS50P week_1

--- week_1/interpreter/interpreter.py
+++ week_1/interpreter/interpreter.py
@@ -1,6 +1,7 @@
 expression = input('Expression: ')
 num_1, operation, num_2 = expression.split(' ')
 
+# all basic types of operations
 if operation == '+':
     print(float(int(num_1) + int(num_2)))
 elif operation == '*':
