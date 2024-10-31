36df823d76be775252c2d9b11bdf4e09a61d4854
yamilobt8 <yamilo12345678910@gmail.com>
2024-10-31 19:48:34 +0100

    CS50P week_4

---
+++ week_4/adieu/adieu.py
@@ -0,0 +1,20 @@
+import sys
+
+names = []
+while True:
+    try:
+        name = input('Name: ')
+        names.append(name)
+    except EOFError:
+        print() # \n
+
+        output = 'Adieu, adieu, to '
+
+        if len(names) == 1:
+            output += names[0]
+        elif len(names) == 2:
+            output += f'{names[0]} and {names[1]}'
+        else:
+            output += ', '.join(names[:-1]) + f', and {names[-1]}'
+        print(output)
+        sys.exit(0)
